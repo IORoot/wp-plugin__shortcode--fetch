9515eef1d3d06130aaf4c46073319687cafc61ac
Andy Pearson <andy@londonparkour.com>
2021-05-29 12:48:19 +0100

    Init

--- src/acf/andyp_plugin_register.php
+++ src/acf/andyp_plugin_register.php
@@ -2,7 +2,7 @@
 
 add_action( 'plugins_loaded', function() {
     do_action('register_andyp_plugin', [
-        'title'     => 'Fetch Shortcode',
+        'title'     => 'Shortcode [fetch]',
         'icon'      => 'allergy',
         'color'     => '#ff0',
         'path'      => __FILE__,
